c6083a8b3b9b3b3919e43c71fede22d558f2bdc0
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2025-05-10 10:33:24 +0545

    Update BaseURL.js

--- frontend/BaseURL.js
+++ frontend/BaseURL.js
@@ -1,3 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000/api/"
+// const BASE_URL = "http://127.0.0.1:8000/api/"
+const BASE_URL = "https://lisi-resturant-management-system.onrender.com/api/"
 
 export default BASE_URL;
