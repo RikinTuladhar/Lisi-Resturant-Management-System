29456457bfc0f58d0b51abe720bf17422602de6f
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2025-06-10 22:10:59 +0545

    Update User.php

--- filament/app/Models/User.php
+++ filament/app/Models/User.php
@@ -5,11 +5,13 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 use App\Enums\UserRole;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -55,7 +57,10 @@ class User extends Authenticatable
         return $this->role === UserRole::ADMIN;
     }
 
-
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->isAdmin();
+    }
 
     public function orders()
     {
